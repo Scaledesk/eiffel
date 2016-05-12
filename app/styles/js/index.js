a61ee3d2ac3254be5f8fbc7a938fdd9d67280dbd
garima <garima8jun@gmail.com>
2016-05-12 11:39:55 +0530

    signup n login update

--- app/styles/js/index.js
+++ app/styles/js/index.js
@@ -22,7 +22,52 @@ $('.next-button.number').click(
   }
 );
 
+$('.password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-lock').addClass("next");
+    } else {
+      $('.icon-lock').removeClass("next");
+    }
+  }
+);
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
 
+$('.next-button.password').click(
+  function(){
+    console.log("Something");
+    $('.password-section').addClass("fold-up");
+    $('.repeat-password-section').removeClass("folded");
+  }
+);
+$('.password').keypress(function(e){
+        if(e.which == 13){//Enter key pressed
+          /*  alert(45555555);*/
+            $('.next-button.password').click();//Trigger search button click event
+        }
+ });
+
+$('.repeat-password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-repeat-lock').addClass("next");
+    } else {
+      $('.icon-repeat-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button.repeat-password').click(
+  function(){
+    console.log("Something");
+    $('.repeat-password-section').addClass("fold-up");
+    $('.success').css("marginTop", 0);
+  }
+);
  $('.number').keypress(function(e){
         if(e.which == 13){//Enter key pressed
           /*  alert(45555555);*/
@@ -55,51 +100,7 @@ $('.next-button.number').click(
     });
 
 
-$('.password').on("change keyup paste",
-  function(){
-    if($(this).val()){
-      $('.icon-lock').addClass("next");
-    } else {
-      $('.icon-lock').removeClass("next");
-    }
-  }
-);
 
-$('.next-button').hover(
-  function(){
-    $(this).css('cursor', 'pointer');
-  }
-);
 
-$('.next-button.password').click(
-  function(){
-    console.log("Something");
-    $('.password-section').addClass("fold-up");
-    $('.repeat-password-section').removeClass("folded");
-  }
-);
 
-$('.password').keypress(function(e){
-        if(e.which == 13){//Enter key pressed
-          /*  alert(45555555);*/
-            $('.next-button.password').click();//Trigger search button click event
-        }
- });
 
-$('.repeat-password').on("change keyup paste",
-  function(){
-    if($(this).val()){
-      $('.icon-repeat-lock').addClass("next");
-    } else {
-      $('.icon-repeat-lock').removeClass("next");
-    }
-  }
-);
-
-$('.next-button.repeat-password').click(
-  function(){
-    console.log("Something");
-    $('.repeat-password-section').addClass("fold-up");
-    $('.success').css("marginTop", 0);
-  }
-);
