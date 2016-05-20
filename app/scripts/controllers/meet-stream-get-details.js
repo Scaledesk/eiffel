7401445ccc8944616cd07d159d2a85c97374ecbe
nkscoder <nkscoder@gmail.com>
2016-05-20 17:28:53 +0530

    meet stream get details form  validation

--- app/scripts/controllers/meet-stream-get-details.js
+++ app/scripts/controllers/meet-stream-get-details.js
@@ -8,10 +8,31 @@
  * Controller of the eiffelApp
  */
 angular.module('eiffelApp')
-  .controller('MeetStreamGetDetailsCtrl', function () {
+  .controller('MeetStreamGetDetailsCtrl', function ($rootScope,$scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+      var _this=this;
+      $rootScope.headerShow = "dashboard";
+
+      $scope.myDate = new Date();
+      $scope.minDate = new Date(
+          $scope.myDate.getFullYear(),
+          $scope.myDate.getMonth() - 2,
+          $scope.myDate.getDate());
+      $scope.maxDate = new Date(
+          $scope.myDate.getFullYear(),
+          $scope.myDate.getMonth() + 2,
+          $scope.myDate.getDate());
+      $scope.onlyWeekendsPredicate = function(date) {
+          var day = date.getDay();
+          return day === 0 || day === 6;
+      }
+
+      _this.submit=function () {
+
+      }
+
   });
