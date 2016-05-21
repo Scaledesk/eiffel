62b046347b1c1db14253d1c2d122e5dec44aee54
nkscoder <nkscoder@gmail.com>
2016-05-21 12:55:24 +0530

    q a get details form validation

--- app/scripts/controllers/q-a-get-details.js
+++ app/scripts/controllers/q-a-get-details.js
@@ -8,10 +8,20 @@
  * Controller of the eiffelApp
  */
 angular.module('eiffelApp')
-  .controller('QAGetDetailsCtrl', function () {
+  .controller('QAGetDetailsCtrl', function ($rootScope,$scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
+        
     ];
+      var _this=this;
+      $rootScope.headerShow = "dashboard";
+      _this.submit=function (){
+          // Set the 'submitted' flag to true
+          $scope.submitted = true;
+
+
+
+          }
   });
